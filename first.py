3118f5990981d786931e2de087635cd7ec4a8c62
jiaqiyusun <jiaqi.yu.sun@gmail.com>
2021-08-11 15:33:11 +0100

    main

--- first.py
+++ first.py
@@ -1,2 +1,24 @@
 #This is my first project python
+#number
 print(520)
+#a machine do not need to know, soon """
+print("hello world")
+print('hello world')
+
+print(3+1)
+
+#create one file for write
+fp=open('/Users/jiaqiyu/Documents/python/file.txt', 'a+')
+fp.write('welcome')
+fp.close()
+
+print('hello\nworld')
+print('hello\tworld')
+print('hello\rworld')
+print('hello\bworld') #lost last one caracts
+print('hello\\l') #just have one \
+print('hello:\'worls\'')
+print(r'hello\nworld') #remove effect  \n
+
+print(chr(000)) #charact chines
+print(ord('A')) #to known de nº ascii
---
+++ variavel.py
@@ -0,0 +1,37 @@
+name = 'maria'
+name = 'joao'
+print(name)
+
+print('id',id(name))
+print('type',type(name))
+print(name)
+
+print(type(98))
+print(type(98.9))
+print(type(True))
+print('binario',0b1111)
+print('oitonario',0o1111)
+print('deseisnario',0o1111)
+
+a = 3.14158
+print(a,type(a))
+
+n1=1.1
+n2=2.2
+
+print(n1+n2)
+
+from decimal import Decimal
+print(Decimal('1.1')+Decimal('2.2'))
+print(True+1)
+
+print('it is number '+str(n1))
+
+name= 'jiaqi'
+#string can not invert int
+print(int(n1))
+print(float(n1))
+
+'''hei comentario'''
+
+#coding:utf-8 mandarim
